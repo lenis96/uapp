18ee963e09adc4b246a74cbeeb04135f81a43c09
lenis96 <lenisandres5@gmail.com>
2022-02-06 18:46:22 -0500

    added tailwind css

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Documentation from '../views/Documentation.vue'
 import Home from '../views/Home.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path:'/',
+    name:'Home',
     component: Home
+  },
+  {
+    path: '/documentation',
+    name: 'Documentation',
+    component: Documentation
   },
   {
     path: '/about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="flex flex-col grid-cols-12 h-screen">
+
+  <div class="col-span-12">
+      <div class="d-flex flex-row">
+          <div class="d-flex flex-row">
+              <a class="mx-2" href="">Home</a>
+              <a class="mx-2" href="">Nosotros</a>
+          </div>
+      </div>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+  </div>
+  <div class="col-span-12 flex-1">
+      <h2>Columna2</h2>
+  </div>
+  <div class="bg-yellow-600 col-span-12">
+      <h2>Columna3</h2>
+</div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
